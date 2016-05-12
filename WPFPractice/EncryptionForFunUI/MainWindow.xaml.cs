538a790c2880cfd1f6f96ba46372a1ecd1bcf6f8
Brad Walbeck <brad.walbeck@pertan.com>
2016-05-12 17:00:27 -0600

    - Added file opening to buttons - Added funtion for saving (not yet wired to buttons)

--- WPFPractice/EncryptionForFunUI/MainWindow.xaml.cs
+++ WPFPractice/EncryptionForFunUI/MainWindow.xaml.cs
@@ -2,6 +2,9 @@
 using System.Windows.Controls;
 using System.Windows.Navigation;
 using System.Diagnostics;
+using Microsoft.Win32;
+using System;
+using System.IO;
 
 namespace EncryptionForFunUI
 {
@@ -45,5 +48,56 @@ namespace EncryptionForFunUI
             e.Handled = true;
         }
 
+        private string LoadFile(string fileDialogTitle)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();       
+            openFileDialog.Multiselect= false;
+            openFileDialog.Title = fileDialogTitle;
+            openFileDialog.Filter = "Text files|*.txt";
+            openFileDialog.FilterIndex = 1;
+
+            Nullable<bool> result = openFileDialog.ShowDialog();
+
+            if (result != true)
+            {
+                return null;
+            }
+
+            return File.ReadAllText(openFileDialog.FileName);
+        }
+
+        private void encrypt_LoadPlainText_Click(object sender, RoutedEventArgs e)
+        {
+            encrypt_plain_text.Text = LoadFile("Open Plain Text");
+        }
+
+        private void encrypt_LoadPublicKey_Click(object sender, RoutedEventArgs e)
+        {
+            encrypt_public_key.Text = LoadFile("Open Public Key");
+        }
+
+        private void decrypt_LoadPrivateKey_Click(object sender, RoutedEventArgs e)
+        {
+            decrypt_private_key.Text = LoadFile("Open Private Key");
+        }
+
+        private void decrypt_LoadCypherText_Click(object sender, RoutedEventArgs e)
+        {
+            decrypt_cypher.Text = LoadFile("Open Cypher Text");
+        }
+
+        private void SaveTextToFile(string fileText, string fileName, string fileDialogTitle) {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = fileDialogTitle;
+            saveFileDialog.ShowDialog();
+            saveFileDialog.FileName = fileName;
+            if (saveFileDialog.FileName != "")
+            {
+                using (StreamWriter sw = new StreamWriter(saveFileDialog.OpenFile()))
+                {
+                    sw.Write(fileText);
+                }
+            }
+        }
     }
 }
